18f1bbc991bae792c577a08c99e9c81ad5a3da44
brunocastroibarburu <brunocastroibarburu@gmail.com>
2023-03-22 01:12:53 +0000

    adding mount to local container

--- local_container.sh
+++ local_container.sh
@@ -1,8 +1,10 @@
 PROJECT_NAME=new-project
-docker build  -t ${PROJECT_NAME}-image   .
+# docker build  -t ${PROJECT_NAME}-image   .
 
+# For Windows with SSH
 winpty docker run --rm -it \
 --mount type=bind,source="$(PWD)",target=/root/project \
+--mount type=bind,source="$HOME/.ssh",target=/root/.ssh \
 --name ${PROJECT_NAME}-container \
 -p 8080:8080 \
 --entrypoint bash \
